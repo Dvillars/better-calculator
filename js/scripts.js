397193e4300091b8c02ead90208e906aef5af6d9
Brandon Cooper and Derek Villars <brandon+derek@>
2017-01-12 13:34:38 -0800

    add a working if statment in combonation with a mouseenter listener

--- js/scripts.js
+++ js/scripts.js
@@ -14,45 +14,82 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-$(document).ready(function() {
-  $("#multiply").click(function(){
+  $(document).ready(function() {
+    var action;
+    $("#multiply").mouseenter(function(){
+      action = 'multiply'
+    });
+    $("#plus").mouseenter(function(){
+      action = 'plus'
+    });
+    $("#subtract").mouseenter(function(){
+      action = 'subtract'
+    });
+    $("#divide").mouseenter(function(){
+      action = 'divide'
+    });
     $("form").submit(function(event) {
       event.preventDefault();
       var number1 = parseInt($("#num1").val());
       var number2 = parseInt($("#num2").val());
-      var result = multiply(number1, number2);
-      $("#output").text(result);
+      if (action === 'multiply') {
+        var result = multiply(number1, number2);
+        $("#output").text(result);
+      } else if (action === 'plus') {
+        var result = plus(number1, number2);
+        $("#output").text(result);
+      } else if (action === 'subtract') {
+        var result = subtract(number1, number2);
+        $("#output").text(result);
+      } else if (action === 'divide') {
+        var result = divide(number1, number2);
+        $("#output").text(result);
+      } else {
+        console.log('Darn  theres an error')
+      }
     });
   });
 
-  $("#plus").click(function(){
-    $("form").submit(function(event) {
-      event.preventDefault();
-      var number1 = parseInt($("#num1").val());
-      var number2 = parseInt($("#num2").val());
-      var result = plus(number1, number2);
-      $("#output").text(result);
-    });
-  });
 
-  $("#subtract").click(function(){
-    $("form").submit(function(event) {
-      event.preventDefault();
-      var number1 = parseInt($("#num1").val());
-      var number2 = parseInt($("#num2").val());
-      var result = subtract(number1, number2);
-      $("#output").text(result);
-    });
-  });
+// $(document).ready(function() {
+//   $("#multiply").click(function(){
+//     $("form").submit(function(event) {
+//       event.preventDefault();
+//       var number1 = parseInt($("#num1").val());
+//       var number2 = parseInt($("#num2").val());
+//       var result = multiply(number1, number2);
+//       $("#output").text(result);
+//     });
+//   });
+//
+//   $("#plus").click(function(){
+//     $("form").submit(function(event) {
+//       event.preventDefault();
+//       var number1 = parseInt($("#num1").val());
+//       var number2 = parseInt($("#num2").val());
+//       var result = plus(number1, number2);
+//       $("#output").text(result);
+//     });
+//   });
+//
+//   $("#subtract").click(function(){
+//     $("form").submit(function(event) {
+//       event.preventDefault();
+//       var number1 = parseInt($("#num1").val());
+//       var number2 = parseInt($("#num2").val());
+//       var result = subtract(number1, number2);
+//       $("#output").text(result);
+//     });
+//   });
+//
+//   $("#divide").click(function(){
+//     $("form").submit(function(event) {
+//       event.preventDefault();
+//       var number1 = parseInt($("#num1").val());
+//       var number2 = parseInt($("#num2").val());
+//       var result = divide(number1, number2);
+//       $("#output").text(result);
+//     });
+//   });
 
-  $("#divide").click(function(){
-    $("form").submit(function(event) {
-      event.preventDefault();
-      var number1 = parseInt($("#num1").val());
-      var number2 = parseInt($("#num2").val());
-      var result = divide(number1, number2);
-      $("#output").text(result);
-    });
-  });
-
-});
+//});
